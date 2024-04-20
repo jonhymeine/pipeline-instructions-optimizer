5f76dbe051ce99d7eb3fab3c5942c6e66f809dc7
Jonathas Meine <jonhy.meine@gmail.com>
2024-04-20 16:13:57 -0300

    refactor: change the way to split the binary instructions

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ const fs = require('fs');
 
 const readline = require('node:readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
-let instructions = [];
+let instructions;
 
 try {
-    const data = fs.readFileSync('./input/input-file.txt', 'utf8');
-    instructions = data.split('\r\n');
+    let data = fs.readFileSync('./input/input-file.txt', 'utf8');
+    data = data.replace(/\r/g, '');
+    instructions = data.split('\n');
     if (instructions[instructions.length - 1] == '') {
         instructions.pop();
     }
     console.log(instructions);
     console.log(instructions[1].substring(25));
     console.log(instructions[1].substring(20, 25));
-    
+    console.log(instructions[1].substring(12, 17));
+    console.log(instructions[1].substring(7, 12));
+
 } catch (error) {
     console.error(error);
 }
