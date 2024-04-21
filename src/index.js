dd87a1f6322b57717c18c6769c78af50020894d1
Jonathas Meine <jonhy.meine@gmail.com>
2024-04-21 19:34:57 -0300

    refactor: change index.js to only call the Program Controller

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
-const fs = require('fs');
+const ProgramController = require('./controllers/ProgramController');
 
-const readline = require('node:readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-const InstructionController = require('./controllers/InstructionController');
-const instructionController = new InstructionController();
-
-try {
-    let data = fs.readFileSync('./input/input-file.txt', 'utf8');
-
-    instructionController.set_instructions(data);
-    console.log(instructionController.get_instructions());
-
-
-} catch (error) {
-    console.error(error);
-}
-
-// rl.question(`Input pipeline's clock time (in nanoseconds): `, time => {
-//   console.log(`Clock time set to ${time} ns`);
-//   rl.close();
-// });
+const program_controller = new ProgramController();
+program_controller.show_option();
