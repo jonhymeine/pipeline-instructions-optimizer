1d1ee8145aeb60da0cfb19fdf2c89f455c72fa27
malockey <malockeybr@gmail.com>
2024-04-27 19:58:54 -0300

    feat: Defines ProgramController class to manage the execution of the program and interaction with the user

--- src/controllers/ProgramController.js
+++ src/controllers/ProgramController.js
@@ -5,7 +5,7 @@ const readline = require('node:readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    terminal: false
+    terminal: false,
 });
 
 const fs = require('fs');
@@ -16,40 +16,82 @@ class ProgramController {
     /**
      * Files in the input folder
      * @type {string[]}
+     * @private
      */
-    files;
+    #files;
+
+    /**
+     * Pipeline's clock time
+     * @type {number}
+     * @private
+     */
+    #clock_time;
+
+    /**
+     * Before solution Performance
+     * @type {{instructions_count: number, cycles_count: number, execution_time: number}}
+     * @private
+     */
+    #before_solution;
+
+    /**
+     * After solution Performance
+     * @type {{instructions_count: number, cycles_count: number, execution_time: number}}
+     * @private
+     */
+    #after_solution;
 
     /**
      * AssemblyProgram object
      * @type {AssemblyProgram}
      * @private
      */
-    #program = new AssemblyProgram();
+    #assembly_program = new AssemblyProgram();
+
+    /**
+     * PerformanceCalculator object
+     * @type {PerformanceCalculator}
+     * @private
+     */
+    #performance_calculator = new PerformanceCalculator();
+
+    /**
+     * Asks the user for the pipeline's clock time
+     * @private
+     * @returns {number} - Pipeline's clock time
+     */
+    #ask_for_clock_time() {
+        try {
+            rl.question("Input the pipeline's clock time (in nanoseconds): ", time => {
+                this.#clock_time = time;
+                this.#request_file();
+            });
+        } catch (error) {
+            console.error('Error reading clock time', error);
+        }
+    }
 
     /**
      * Exports the solution to a file
      * @param {string} input_file_name - Input file name
      * @private
-    */
+     */
+
     #export_solution(input_file_name) {
         try {
-            if (this.files.includes(input_file_name) == false) {
-                console.clear();
-                console.error('Invalid file name');
-                this.show_option();
-                return;
-            }
             const data = fs.readFileSync(`${input_folder}${input_file_name}`, 'utf8');
-            this.#program.set_instructions(data);
-            this.#program.add_nops_to_instructions();
+            this.#assembly_program.set_instructions(data);
+
+            this.#before_solution = this.#performance_calculator.calculate_performance(this.#assembly_program, this.#clock_time);
+            this.#assembly_program.add_nops_to_instructions();
+            this.#after_solution = this.#performance_calculator.calculate_performance(this.#assembly_program, this.#clock_time);
 
             const output_file_name = input_file_name.replace('.txt', '_output.txt');
-            const content = this.#program.get_raw_instructions();
+            const content = this.#assembly_program.get_raw_instructions();
             fs.writeFileSync(`${output_folder}${output_file_name}`, content);
             console.clear();
-            console.log(`Output file created: ${output_folder}${output_file_name}`);
-            this.show_option();
-
+            this.#show_results(`${output_folder}${output_file_name}`);
+            this.#request_file();
         } catch (err) {
             console.error(err);
         }
@@ -57,22 +99,30 @@ class ProgramController {
 
     /**
      * Asks the user to select a file
+     * @private
      */
-    show_option() {
+    async #request_file() {
         try {
-            this.files = fs.readdirSync(input_folder);
+            this.#files = fs.readdirSync(input_folder);
             let question = 'Select a file:\n';
-            this.files.forEach((file, index) => {
+            this.#files.forEach((file, index) => {
                 question += `${index + 1} - ${file}\n`;
             });
             question += 'X - Exit\n\nOption: ';
 
-            rl.question(question, option => {
+            await rl.question(question, option => {
                 if (option == 'X' || option == 'x') {
                     rl.close();
                     return;
                 }
-                const file_name = this.files[option - 1];
+                const file_name = this.#files[option - 1];
+                if (!this.#files.includes(file_name)) {
+                    console.clear();
+                    console.error('Invalid file');
+                    this.#request_file();
+                    return;
+                }
+
                 this.#export_solution(file_name);
             });
         } catch (error) {
@@ -81,17 +131,41 @@ class ProgramController {
     }
 
     /**
-     * Asks the user for the pipeline's clock time
+     * Calculate the performance
+     * @param {string} output_path - Output file path
      * @private
-     * @returns {number} - Pipeline's clock time
      */
-    #ask_for_clock_time() {
-        let clock_time;
-        rl.question("Input the pipeline's clock time (in nanoseconds): ", time => {
-            clock_time = time;
-            rl.close();
+    #show_results(output_path) {
+        [this.#before_solution, this.#after_solution].forEach((solution, index) => {
+            if (index == 0) {
+                console.log('Ideal pipeline:');
+            } else {
+                console.log('After solution:');
+            }
+            console.log(
+                'Instructions:',
+                solution.instructions_count,
+                '\nCycles:',
+                solution.cycles_count,
+                '\nExecution time:',
+                solution.execution_time,
+                'ns\n'
+            );
         });
-        return clock_time;
+
+        let performance_comparison = this.#performance_calculator.compare_performance(this.#before_solution, this.#after_solution);
+        performance_comparison = parseFloat(performance_comparison.toFixed(4));
+        console.log(`The ideal pipeline is ${performance_comparison} times faster than the NOP's solution`);
+
+        console.log(`Output file created: ${output_path}`);
+    }
+
+    /**
+     * Run the Program
+     */
+    run() {
+        console.clear();
+        this.#ask_for_clock_time();
     }
 }
 
