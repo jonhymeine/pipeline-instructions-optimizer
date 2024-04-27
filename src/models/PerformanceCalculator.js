d8ca81bb842e583a85511fb0be4dd076d227732c
malockey <malockeybr@gmail.com>
2024-04-27 19:51:58 -0300

    feat: Adds PerformanceCalculator class with methods to calculate and compare performance

--- src/models/PerformanceCalculator.js
+++ src/models/PerformanceCalculator.js
@@ -1,23 +1,34 @@
 const AssemblyProgram = require('./AssemblyProgram');
 
-const readline = require('node:readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
 class PerformanceCalculator {
     /**
      * Calculates program performance
-     * @param {AssemblyProgram} program - AssemblyProgram object
+     * @param {AssemblyProgram} assembly_program - AssemblyProgram object
      * @param {number} clock_time - Pipeline's clock time
-    */
-    calculate_performance(program, clock_time) {
-        const instructions_count = program.get_instructions().length;
+     * @returns {Object} - Object containing the number of cycles and the execution time
+     */
+    calculate_performance(assembly_program, clock_time) {
+        const instructions_count = assembly_program.get_instructions().length;
         const cycles_count = instructions_count + 4;
         const execution_time = cycles_count * clock_time;
 
-        console.log(`Cycles count: ${cycles_count}\nExecution time: ${execution_time} nanoseconds`);
+        return {
+            instructions_count,
+            cycles_count,
+            execution_time,
+        };
+    }
+
+    /**
+     * Compare two performances, with performance_b being [performance] times faster than performance_a
+     * @param {{instructions_count: number, cycles_count: number, execution_time: number}} performance_a - Performance object
+     * @param {{instructions_count: number, cycles_count: number, execution_time: number}} performance_b - Performance object
+     * @returns {number} - Performance comparison
+     */
+    compare_performance(performance_a, performance_b) {
+        const performance = performance_b.execution_time / performance_a.execution_time;
+
+        return performance;
     }
 }
 
