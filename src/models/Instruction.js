4d92a8136d0ad95f48c7d5ed212f5ae936f401e8
Jonathas Meine <jonhy.meine@gmail.com>
2024-05-13 11:30:14 -0300

    feat: add instruction immediate binary and decimal attributes and a method to covert from binary to decimal

--- src/models/Instruction.js
+++ src/models/Instruction.js
@@ -1,39 +1,57 @@
 class Instruction {
     /**
      * @type {string}
-     Raw instruction
-    */
+     * Raw instruction
+     */
     raw_instruction;
 
     /**
      * @type {string}
-     Instruction opcode
-    */
+     * Instruction opcode
+     */
     opcode;
 
     /**
      * @type {string}
-     Instruction format
-    */
+     * Instruction format
+     */
     format;
 
     /**
      * @type {string}
-     Instruction rd
-    */
+     * Instruction rd
+     */
     rd;
 
     /**
      * @type {string}
-     Instruction rs1
-    */
+     * Instruction rs1
+     */
     rs1;
 
     /**
      * @type {string}
-     Instruction rs2
-    */
+     * Instruction rs2
+     */
     rs2;
+
+    /**
+     * @type {string}
+     * Binary immediate value
+     */
+    binary_immediate;
+
+    /**
+     * @type {number}
+     * Decimal immediate value
+     */
+    decimal_immediate;
+
+    /**
+     * @type {Instruction}
+     * Branch target
+     */
+    branch_target;
 
     /**
      * @param {string} instruction - RISC-V assembly binary instruction
@@ -56,21 +74,57 @@ class Instruction {
             this.format = 'I';
             this.rd = instruction.substring(20, 25);
             this.rs1 = instruction.substring(12, 17);
+            this.binary_immediate = instruction.substring(0, 12);
+            this.decimal_immediate = this.#binary_to_decimal(this.binary_immediate);
         } else if (this.opcode == '1100011') {
             this.format = 'B';
             this.rs1 = instruction.substring(12, 17);
             this.rs2 = instruction.substring(7, 12);
+            this.binary_immediate =
+                instruction.substring(0, 1) +
+                instruction.substring(24, 25) +
+                instruction.substring(1, 7) +
+                instruction.substring(20, 24) +
+                '0';
+            this.decimal_immediate = this.#binary_to_decimal(this.binary_immediate);
         } else if (this.opcode == '0100011') {
             this.format = 'S';
             this.rs1 = instruction.substring(12, 17);
             this.rs2 = instruction.substring(7, 12);
+            this.binary_immediate = instruction.substring(0, 7) + instruction.substring(20, 25);
+            this.decimal_immediate = this.#binary_to_decimal(this.binary_immediate);
         } else if (this.opcode == '0110111' || this.opcode == '0010111') {
             this.format = 'U';
             this.rd = instruction.substring(20, 25);
+            this.binary_immediate = instruction.substring(0, 20);
+            this.decimal_immediate = this.#binary_to_decimal(this.binary_immediate);
         } else if (this.opcode == '1101111') {
             this.format = 'J';
             this.rd = instruction.substring(20, 25);
+            this.binary_immediate =
+                instruction.substring(0, 1) +
+                instruction.substring(12, 20) +
+                instruction.substring(11, 12) +
+                instruction.substring(1, 11) +
+                '0';
+            this.decimal_immediate = this.#binary_to_decimal(this.binary_immediate);
         }
+    }
+
+    /**
+     * Convert binary to decimal
+     * @param {string} binary - Binary number
+     * @returns {number} - Decimal number
+     */
+    #binary_to_decimal(binary) {
+        let extended_binary;
+
+        if (binary[0] == '1') {
+            extended_binary = binary.padStart(32, 1);
+        } else {
+            extended_binary = binary.padStart(32, 0);
+        }
+        return ~~Number.parseInt(extended_binary, 2);
     }
 }
 
